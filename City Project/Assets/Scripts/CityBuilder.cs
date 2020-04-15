807c7c0e7f6a9d45e840632a6f259e66d12a59c2
MatthiasYo <matthiasgreferath101@gmail.com>
2020-04-15 19:48:41 +0100

    Initial code and assets

---
+++ City Project/Assets/Scripts/CityBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CityBuilder : MonoBehaviour
+{
+    public GameObject[] cityBlock;
+
+    void Start()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                Instantiate(cityBlock[(int)Random.Range(0,5)], new Vector3(j, 0, i), Quaternion.identity);
+            }
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
